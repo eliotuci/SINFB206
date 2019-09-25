c0799eb603d70cdb7b8ee7789a15b630920e3fb3
Simon Genin <SimonGenin@users.noreply.github.com>
2019-09-25 15:53:33 +0200

    Avoid overlapping agents on init state

--- sources/agents.c
+++ sources/agents.c
@@ -1,5 +1,19 @@
 #include "../headers/agents.h"
 
+
+void avoid_overlapping_agents_on_starting_state() {
+    for (int a = 0; a < NUM_AGENTS_TYPE_1; a++) {
+        for (int aa = 0; aa < NUM_AGENTS_TYPE_1; aa++) {
+            if (a == aa) continue;
+            if (agent_type_1[a].x_pos == agent_type_1[aa].x_pos && agent_type_1[a].y_pos == agent_type_1[aa].y_pos) {
+                agent_type_1[a].x_pos = gsl_rng_uniform_int(r_rand, NUM_CELLS_PER_COLUMN);
+                agent_type_1[a].y_pos = gsl_rng_uniform_int(r_rand, NUM_CELLS_PER_COLUMN);
+                aa -= 1;
+            }
+        }
+    }
+}
+
 void init_agents_position_and_colour( void ){
   for( int a = 0; a < NUM_AGENTS_TYPE_1; a++){
     agent_type_1[a].x_pos = gsl_rng_uniform_int(r_rand, NUM_CELLS_PER_COLUMN );
@@ -18,6 +32,7 @@ void init_agents_position_and_colour( void ){
     agent_type_2[a].colors[2] = TYPE_2_BLUE;
     agent_type_2[a].shape     = SHAPE_OF_AGENTS_TYPE_2;
   }
+  avoid_overlapping_agents_on_starting_state()
 }
 
 extern void update_agents_positions ( void ){
